ae908c1bf8b4b6231242a97d12ef06cac7270f2c
Mariana Martinez <charitmarianam@gmail.com>
2026-08-24 17:55:07 -0500

    Ordenando

--- config/database.php
+++ config/database.php
@@ -1,23 +1,41 @@
 <?php
+declare(strict_types=1);
 
 class Database {
-    private $host = "localhost";
-    private $db_name = "multiservicios_omega";
-    private $username = "root";
-    private $password = "";
-    public $conn;
+    private $host;
+    private $port;
+    private $dbname;
+    private $username;
+    private $password;
+
+    public function __construct() {
+        $env = parse_ini_file(__DIR__ . '/../.env');
+
+        $this->host = $env['DB_HOST'];
+        $this->port = $env['DB_PORT'];
+        $this->dbname = $env['DB_NAME'];
+        $this->username = $env['DB_USER'];
+        $this->password = $env['DB_PASSWORD'];
+    }
+
+    public function conectar() {
+        return new PDO(
+            "mysql:host={$this->host};port={$this->port};dbname={$this->dbname};charset=utf8mb4",
+            $this->username,
+            $this->password,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
+    }
+
+    public function connect() {
+        return $this->conectar();
+    }
 
     public function getConnection() {
-        $this->conn = null;
-
-        try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $exception) {
-            echo "Error de conexión: " . $exception->getMessage();
-        }
-
-        return $this->conn;
+        return $this->conectar();
     }
 }
+?>
