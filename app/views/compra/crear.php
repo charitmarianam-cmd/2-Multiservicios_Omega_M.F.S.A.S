c28febd5a7db332ec7f06e5663f4351516a90e14
Mariana Martinez <charitmarianam@gmail.com>
2026-09-24 17:43:04 -0500

    crear de formularios para metodos post

---
+++ app/views/compra/crear.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="compra/crear.php" method="POST">
+
+<input type="text" name="id">
+<input type="text" name="id_proveedor">
+<input type="text" name="fecha">
+<input type="text" name="total">
+<button type="submit">Guardar</button>
+
+</form>
+
+</body>
+</html>
+
+
+
---
+++ app/views/proveedor/crear.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="proveedor/crear.php" method="POST">
+
+<input type="text" name="id">
+<input type="text" name="nombre">
+<input type="text" name="nit">
+<input type="text" name="telefono">
+<input type="text" name="direccion">
+<button type="submit">Guardar</button>
+
+</form>
+
+</body>
+</html>
+
+
